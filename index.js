09933ace3078a17d4eb70d8614db1106f944be71
Lincon11 <eibraham37@gmail.com>
2022-10-11 14:40:50 -0500

    Programa finalizado

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 var scanf = require('scanf');
+
+console.log('Mi Cadena de Texto');
+console.log('\n');
+console.log('Acontinuacion escriba lo que desee y al terminar escriba "cancelar" para ver en consola lo que escribio.');
+console.log('\n');
+
+var cadenaDeTexto = [];
+var cancelar 
+
+do {
+
+var listaDePalabras = scanf("%s");
+cancelar = listaDePalabras;
+cadenaDeTexto.push(cancelar);
+    
+} while (cancelar != "cancelar");
+
+cadenaDeTexto.pop();
+console.log('\n');
+console.log('Finalizaste tu texto, ahora podras verlo:')
+console.log('\n');
+cadenaDeTexto.forEach(cancelar => {
+    console.log(`${cancelar}`)
+    
+});
